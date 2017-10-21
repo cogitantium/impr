cd5b1fe3068b9a569e8ac3e67889b967874445e0
cogitantium <falke.cs@gmail.com>
2017-10-21 16:26:30 +0200

    final revision of assignment5

--- assignments/assignment5/atm.c
+++ assignments/assignment5/atm.c
@@ -15,11 +15,11 @@ void separator(int *withdrawnBills);
 int main(void) {
   int requestWithdraw, withdrawnBills, billsHundred, billsFifty, billsTwenty, billsTen;
 
-  /* keeps scanning for input, until user requests a non-legative withdrawal amount divisible by 10 */
+  /* keeps scanning for input, until user requests a non-negative withdrawal amount divisible by 10 */
   do {
     printf("Input non-negative amount divisible with 10, please: ");
     scanf(" %d",&requestWithdraw);
-  } while(requestWithdraw<0 || !requestWithdraw%10);
+  } while(requestWithdraw<0 || requestWithdraw%10);
 
   /* calls function for calculating the amount of individual bills for each category */
   calculateBillAmount(requestWithdraw, &billsHundred, &billsFifty, &billsTwenty, &billsTen);
@@ -27,6 +27,8 @@ int main(void) {
   /* calculates number of categories that exist, using NOT false evaluation */
   withdrawnBills = !!billsHundred + !!billsFifty + !!billsTwenty + !!billsTen;
 
+  /* we nicely print the requested amount, individual amount of bills along with
+   taking proper grammar into account and omitting non-relevant sizes of bills */
   printf("You've requested a withdrawal of $%d, we'll give you: ",requestWithdraw);
   if (billsHundred) {
     printf("%d hundred dollar %s",billsHundred,billsHundred>1?"bills":"bill");
@@ -44,6 +46,7 @@ int main(void) {
   return EXIT_SUCCESS;
 }
 
+/* we create a function for returning the amount of bills for each size */
 void calculateBillAmount(int requestWithdraw, int *billsHundred, int *billsFifty, int *billsTwenty, int *billsTen) {
   int remainder = requestWithdraw;
   *billsHundred = calculateSingleAmount(&remainder,HUNDRED);
@@ -52,12 +55,13 @@ void calculateBillAmount(int requestWithdraw, int *billsHundred, int *billsFifty
   *billsTen = calculateSingleAmount(&remainder,TEN);
 }
 
+/* we create a function for calculating the amount of bills for each size */
 int calculateSingleAmount(int *remainder, int sizeBill) {
   int tempRemainder = *remainder/sizeBill;
   *remainder = *remainder%sizeBill;
   return tempRemainder;
 }
-
+/* we create a function for setting proper punctuation between the print-statements */
 void separator(int *withdrawnBills) {
   (*withdrawnBills)--;
   if (*withdrawnBills>=2) {
