b94e32ac46dbbcc1a0d23766476be82c823c23b7
cogitantium <falke.cs@gmail.com>
2017-10-25 22:55:19 +0200

    completed assign6

--- assignments/assignment6/integral.c
+++ assignments/assignment6/integral.c
@@ -1,3 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+double trap(double a, double b, int n, double (*f)(double));
+double g(double x);
+double h(double x);
+
+int main(void) {
+  int n;
+  printf("Calculating the area of g(x)=x^2*sin(x) in the interval [0,3.14159]\n");
+  for (n=2; n<=128; n*=2) {
+    printf("When using n = %3d, the area of g(x) is: %lf\n", n, trap(0, 3.14159, n, g));
+  }
+
+  printf("\nCalculating the area of f(x)=sqrt(4-x^2) in the interval [-2,2]\n");
+  for (n=2; n<=128; n*=2) {
+    printf("When using n = %3d, the area of h(x) is: %lf\n", n, trap(-2, 2, n, h));
+  }
+  return EXIT_SUCCESS;
+}
+
+double trap(double a, double b, int n, double (*f)(double)) {
+  int i;
+  double area = f(a)+f(b), h = (b-a)/n;
+
+  for (i=1; i<n; i++) {
+    double xi = a+i*h;
+    area += 2*f(xi);
+  }
+  return area *= h/2;
+}
+
+double g(double x) {
+  return x*x*sin(x);
+}
+
+double h(double x) {
+  return sqrt(4-x*x);
+}
