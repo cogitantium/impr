8b18027145119a5af769e0d67aa780723dcb2a36
cogitantium <falke.cs@gmail.com>
2017-10-30 15:56:46 +0100

    assign7 functionally finalized and commented

--- assignments/assignment7/mergearray.c
+++ assignments/assignment7/mergearray.c
@@ -1,35 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /* prototyping of functions*/
-double* mergeArray(double *arrFirst, double *arrSecond);
+double* mergeArray(double *arrFirst, double *arrSecond, int worstCase, int arrFirstSize, int arrSecondSize);
 
 int main(void) {
   int i;
   /* create and initialize sorted arrays */
   double arrFirst[5] = {-10.5, -1.8, 3.5, 6.3, 7.2};
   double arrSecond[3] = {-1.8, 3.1, 6.3};
-
-  /* double *arrMerge = (double*)malloc(worstCase); */
-  double *arrMerge = mergeArray(arrFirst, arrSecond);
-  /* */
+  /* calculating worst-case resulting array-size and elements in first and second array */
+  int worstCase = ((sizeof(arrFirst) + sizeof(arrSecond)) / sizeof(double));
+  int arrFirstSize = sizeof(arrFirst) / sizeof(double);
+  int arrSecondSize = sizeof(arrSecond) / sizeof(double);
+  /* assigning resulting array from function to arrMerge */
+  double *arrMerge = mergeArray(arrFirst, arrSecond, worstCase, arrFirstSize, arrSecondSize);
+  /* testing print - should be handled in separate function */
   for (i=sizeof(arrMerge)/sizeof(double); i>0; i--) {
     printf("%f\n", arrMerge[i]);
   }
   return EXIT_SUCCESS;
 }
 
-double* mergeArray(double *arrFirst, double *arrSecond) {
+double* mergeArray(double *arrFirst, double *arrSecond, int worstCase, int arrFirstSize, int arrSecondSize) {
   int i, m = 0, n = 0, p = 0;
-  printf("%d\n", sizeof(arrFirst));
-  int worstCase = ((sizeof(arrFirst) + sizeof(arrSecond)) / sizeof(double));
-  /* ISSUE HERE */
+  /* dynamically allocating memory for temporary merging-array,
+  as the number of resulting elements aren't known beforehand */
   double *tempMerge = (double*)malloc(worstCase * sizeof(double));
+  /* prototyping resulting array for later memory allocation */
   double *arrMerge;
-  int arrFirstSize = sizeof(arrFirst) / sizeof(double);
-  int arrSecondSize = sizeof(arrSecond) / sizeof(double);
 
+  /* continue merging until worst-case array-size has been reached */
   while (p < worstCase) {
+    /* handling two out-of-bounds cases for indexes to arrFirst and arrSecond */
     if (m > arrFirstSize) {
       tempMerge[p] = arrSecond[n];
       n++;
@@ -37,22 +39,25 @@ double* mergeArray(double *arrFirst, double *arrSecond) {
       tempMerge[p] = arrFirst[m];
       m++;
     } else if (arrFirst[m] < arrSecond[n]) {
-      /* put arrFirst place m into tempMerge */
+      /* assign arrFirst at index m to tempMerge at index p */
       tempMerge[p] = arrFirst[m];
       m++;
     } else if (arrFirst[m] > arrSecond[n]) {
-      /* put arrSecond place n into tempMerge */
+      /* assign arrSecond at index n to tempMerge at index p */
       tempMerge[p] = arrSecond[n];
       n++;
-    } else if (arrFirst[m] == arrSecond[n]){
-      /* put arrFirst place m into tempMerge */
+    } else if (arrFirst[m] == arrSecond[n]) {
+      /* assign arrFirst at index m to tempMerge at index p */
       tempMerge[p] = arrFirst[m];
+      /* count up both indexes, as we've reached a duplicate */
       m++; n++;
     }
+    /* count tempMerge-array index up, as this element has been handled */
     p++;
   }
-  /* create pointer-array with tempMerge elements*/
+  /* allocate memory for resulting array, as number of elements are now known */
   arrMerge = (double*)malloc(p*sizeof(double));
+  /* for each element in tempMerge, assign respective value to arrMerge to copy */
   for (i=p; i<=0; i--) {
     arrMerge[i] = tempMerge[i];
   }
