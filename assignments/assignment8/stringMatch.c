d212938da3c8a3cf06aeb90207156f529962f222
cogitantium <falke.cs@gmail.com>
2017-11-02 19:35:42 +0100

    updated assign8 with discrete print-function

--- assignments/assignment8/stringMatch.c
+++ assignments/assignment8/stringMatch.c
@@ -9,31 +9,35 @@ void translateCode (char *code, char *warehouse, char *product, char *qualifier)
 void printCode(char *code, char *warehouse, char *product, char *qualifier);
 
 int main(void) {
-  char  question, scanString,
+  /* initialize desired arrays with constant MAXCODELENGTH for length */
+  char  question[1],
+        scanString[MAXCODELENGTH],
         code[MAXCODELENGTH],
         warehouse[MAXCODELENGTH],
         product[MAXCODELENGTH],
         qualifier[MAXCODELENGTH];
   int scanCustom;
 
+  /* ask user for custom code */
   do {
     printf("Do you want to scan a product code? If no, we'll use an example (y/n) ");
-    scanf(" %c", &question);
-    scanCustom = (question == 'y');
-  } while (strlen(&question) == 0);
+    scanf(" %c", question);
+    scanCustom = (*question == 'y');
+  } while(strlen(question) == 0);
 
+  /* scan for custom code if desired by user */
   if (scanCustom) {
-
     do {
-      printf("Scanning custom barcode: ");
-      scanf("%s", &scanString);
-    } while(!strlen(&scanString));
-    strcpy(code, &scanString);
-
+      printf("Input custom barcode (max length is: %d): ", MAXCODELENGTH);
+      scanf("%s", scanString);
+    } while(!strlen(scanString));
+    strcpy(code, scanString);
   } else {
+    /* use example code if no custom code is desired*/
     strcpy(code, "ATL1203S14");
   }
 
+  /* invoke functions for translating and printing code nicely */
   translateCode(code, warehouse, product, qualifier);
   printCode(code, warehouse, product, qualifier);
 
@@ -42,12 +46,14 @@ int main(void) {
 
 void translateCode (char *code, char *warehouse, char *product, char *qualifier) {
   int i, n=0;
+  /* search for warehouse location in string */
   while(isalpha(code[i])) {
     i++;
   }
   strncpy(warehouse, code, i);
   warehouse[i] = '\0';
 
+  /* search for product location in string */
   int start = i;
   while(isdigit(code[i])) {
     i++; n++;
@@ -55,11 +61,10 @@ void translateCode (char *code, char *warehouse, char *product, char *qualifier)
   strncpy(product, &code[start], n);
   product[n] = '\0';
 
-
+  /* calculating qualifier location in string */
   int lengthQualifier = strlen(code) - i;
   strncpy(qualifier, &code[i], lengthQualifier);
   qualifier[lengthQualifier] = '\0';
-
 }
 
 void printCode(char *code, char *warehouse, char *product, char *qualifier) {
