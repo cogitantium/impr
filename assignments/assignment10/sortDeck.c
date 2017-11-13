120be4199bc6580fbd38fbbb27aca89d35963ca5
cogitantium <falke.cs@gmail.com>
2017-11-13 21:17:01 +0100

    finalized printing and improved deck initializing in assign10

--- assignments/assignment10/sortDeck.c
+++ assignments/assignment10/sortDeck.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define DECKSIZE 54
-/* zero-indexed */
-#define SUITSIZE 3
-#define VALUESIZE 12
+#define JOKERS 3
+#define DECKSIZE 52
+#define SUITSIZE 4
+#define VALUESIZE 13
 
 struct card {
   /* allowing space for null-character */
@@ -22,61 +22,59 @@ int printDeck(void);
 
 int main(void) {
   initializeDeck();
+  printf("Printing initialized deck nicely\n");
   printDeck();
 
   return EXIT_SUCCESS;
 }
 
 int initializeDeck(void) {
-  int i=0, s, v, n;
-  /* for each suit {0..3}  */
-  for (s=0; s<=SUITSIZE; s++) {
-    for (v=0; v<=VALUESIZE; v++) {
+  int s, v, n;
+  /* for each suit {0..3} assigning {0.12} to all cards in suit */
+  for (s=0; s<SUITSIZE; s++) {
+    for (v=0; v<VALUESIZE; v++) {
       deck[s*VALUESIZE+v].suit = s;
       deck[s*VALUESIZE+v].value = v;
-      printf("Card number: %2d \thas .value=%2d \t\tand .suit=%d\n", i+1, deck[s*VALUESIZE+v].value, deck[s*VALUESIZE+v].suit); /* debug */
-      i++;
     }
   }
-
-  for (n=i; n<=DECKSIZE; n++) {
+  /* assigning deck.suit to "4" for last three cards to represent jokers */
+  for (n=DECKSIZE; n<DECKSIZE+JOKERS; n++) {
     deck[n].suit = 4;
-    printf("Card number: %d\t\thas .value:%d \t\tand .suit:%d\n", n+1, deck[n].value, deck[n].suit);
   }
   return EXIT_SUCCESS;
 }
 
-/* clubs -> diamonds -> hearts -> spades */
-/* 0=2->8=10, 9=J, 10=Q, 11=K, 12=A */
-
 int printDeck(void) {
   int i;
-  for (i=0; i<=DECKSIZE; i++) {
+  for (i=0; i<DECKSIZE+JOKERS; i++) {
+
     if (deck[i].suit == 4)
       printf("J");
     else {
 
-    if(deck[i].value<=8)
-      printf("%d", deck[i].value+2);
-    else if (deck[i].value == 9)
-      printf("J");
-    else if (deck[i].value == 10)
-      printf("Q");
-    else if (deck[i].value == 11)
-      printf("K");
-    else if (deck[i].value == 12)
-      printf("A");
+      if(deck[i].value<=8 && deck[i].suit<4)
+        printf("%d", deck[i].value+2);
+      else if (deck[i].value == 9)
+        printf("J");
+      else if (deck[i].value == 10)
+        printf("Q");
+      else if (deck[i].value == 11)
+        printf("K");
+      else if (deck[i].value == 12)
+        printf("A");
 
-    switch (deck[i].suit) {
-      case 0: printf("c"); break;
-      case 1: printf("d"); break;
-      case 2: printf("h"); break;
-      case 3: printf("s"); break;
+      switch (deck[i].suit) {
+        case 0: printf("c"); break;
+        case 1: printf("d"); break;
+        case 2: printf("h"); break;
+        case 3: printf("s"); break;
+      }
     }
-    }
-
-    if (i%12 == 0 && i>0)
+    /* printing deck sequentially, linebreaking each 13th card */
+    printf(" ");
+    if ((i+1)%13 == 0 && i>0)
       printf("\n");
-  } printf("\n");
+  }
+  printf("\n");
   return EXIT_SUCCESS;
 }
