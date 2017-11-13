c4bea0325f4855d12dd574bb6778b0b18e0f00bd
cogitantium <falke.cs@gmail.com>
2017-11-14 00:52:24 +0100

    implemented Fisher-Yates in shuffleDeck() prototype

--- assignments/assignment10/sortDeck.c
+++ assignments/assignment10/sortDeck.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #define JOKERS 3
 #define DECKSIZE 52
 #define SUITSIZE 4
 #define VALUESIZE 13
 
 struct card {
-  /* allowing space for null-character */
   int value;
   int suit;
 };
-struct card deck[DECKSIZE];
+struct card deck[DECKSIZE+JOKERS];
 
 /* clubs -> diamonds -> hearts -> spades */
 /* 0=2->8=10, 9=J, 10=Q, 11=K, 12=A */
 
-/* use qsort with rand()-cmpfunc for shuffling */
-
 int initializeDeck(void);
-int printDeck(void);
+void printDeck(void);
+void shuffleDeck();
+int sortDeck();
+int compareCards();
 
 int main(void) {
   initializeDeck();
   printf("Printing initialized deck nicely\n");
   printDeck();
-
+  shuffleDeck();
   return EXIT_SUCCESS;
 }
 
@@ -44,12 +45,12 @@ int initializeDeck(void) {
   return EXIT_SUCCESS;
 }
 
-int printDeck(void) {
+void printDeck(void) {
   int i;
   for (i=0; i<DECKSIZE+JOKERS; i++) {
 
     if (deck[i].suit == 4)
-      printf("J");
+      printf("J ");
     else {
 
       if(deck[i].value<=8 && deck[i].suit<4)
@@ -72,9 +73,28 @@ int printDeck(void) {
     }
     /* printing deck sequentially, linebreaking each 13th card */
     printf(" ");
-    if ((i+1)%13 == 0 && i>0)
+    if ((i+1)%VALUESIZE == 0 && i>0)
       printf("\n");
   }
   printf("\n");
-  return EXIT_SUCCESS;
+}
+
+void shuffleDeck () {
+  /*  To shuffle an array a of n elements (indices 0..n-1):
+  for i from 0 to n−2 do
+  n ← random integer such that i ≤ n < n
+  exchange a[i] and a[n]
+
+  rand() % (max_number + 1 - minimum_number) + minimum_number */
+  srand(time(NULL));
+  struct card temp;
+  int i, n;
+
+  for (i=0; i<=DECKSIZE+JOKERS-2; i++) {
+    n = rand() % ((DECKSIZE+JOKERS-2) + 1 - i) + i;
+    temp = deck[n];
+    deck[n] = deck[i];
+    deck[i] = temp;
+  }
+  printDeck();
 }
