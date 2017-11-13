617dbbd92e3e58788cd49d8e13a6d47e4cab8169
cogitantium <falke.cs@gmail.com>
2017-11-13 19:06:55 +0100

    fixed initializing of deck including jokers in assign10

--- assignments/assignment10/sortDeck.c
+++ assignments/assignment10/sortDeck.c
@@ -27,20 +27,24 @@ int main(void) {
 }
 
 int initializeDeck(void) {
-  int i=0, s, v;
+  int i=0, s, v, n;
   /* for each suit {0..3}  */
   for (s=0; s<=SUITSIZE; s++) {
     for (v=0; v<=VALUESIZE; v++) {
-      deck[s*12+v].suit = s;
-      deck[s*12+v].value = v;
-      printf("Card number: %2d \thas .value=%2d \t\tand .suit=%d\n", i, deck[s*12+v].value, deck[s*12+v].suit);
+      deck[s*VALUESIZE+v].suit = s;
+      deck[s*VALUESIZE+v].value = v;
+      printf("Card number: %2d \thas .value=%2d \t\tand .suit=%d\n", i, deck[s*VALUESIZE+v].value, deck[s*VALUESIZE+v].suit);
       i++;
     }
   }
-  for (v=i; v<=2; v++) {
-    deck[i].suit = 4;
-          printf("Card number: %2d \thas .value=%2d \t\tand .suit=%d\n", i, deck[s*12+v].value, deck[s*12+v].suit);
-          i++;
+  printf("I:%d\n", i);
+
+  for (n=i; n<=DECKSIZE; n++) {
+    deck[n].suit = 4;
+    printf("Card number: %d\t\thas .suit:%d\n", n, deck[n].suit);
   }
+  printf("I:%d\n", i);
+
+
   return EXIT_SUCCESS;
 }
